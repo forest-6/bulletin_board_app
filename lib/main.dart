e7145c5bb93f594b11a0b67010d1067ddf50cc9b
seungmin lim <seungmin3238@gmail.com>
2023-09-12 23:52:34 +0900

    Feat: local json파일 핸들링 진행중
    현재 문제는 함수가 무한호출 되고있다.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -16,16 +18,34 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
+      home: const HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<dynamic> items = [];
+  Future<void> readJson() async {
+    final String response = await rootBundle.loadString('assets/json/bulletin.json');
+    final data = await json.decode(response);
+    setState(() {
+      items = data;
+    });
+    debugPrint('${items.isNotEmpty ? items[0]['title'] : 'No data'}');
+  }
 
   @override
   Widget build(BuildContext context) {
+    readJson();
+    
     return Scaffold( 
       appBar: AppBar(
         title: Image.asset('assets/images/bowling_logo.png',
@@ -34,22 +54,23 @@ class MyHomePage extends StatelessWidget {
       ),
       body: Container(
         margin: const EdgeInsets.fromLTRB(20, 0, 20, 0),
-        child: Column(
-          children: [
-            Container(
-              height: 130,
-              margin: const EdgeInsets.fromLTRB(0, 30, 0, 20),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.blue,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                height: 130,
+                margin: const EdgeInsets.fromLTRB(0, 30, 0, 20),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.blue,
+                ),
               ),
-            ),
-            Bulletins(title: '최신 게시글'),
-            Bulletins(title: '인기 게시글'),
-          ],
-        ),
+              Bulletins(title: '최신 게시글'),
+              Bulletins(title: '인기 게시글'),
+            ],
+          )
+        )
       ),
-      
     );
   }
 }
@@ -76,6 +97,7 @@ class Bulletins extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     return  Column(
       children: [
         Row(
@@ -98,61 +120,47 @@ class Bulletins extends StatelessWidget {
         ),
         Container(
           padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+          margin: const EdgeInsets.fromLTRB(0, 0, 0, 30),
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(10),
             color: const Color(0xffF0F0F0),
           ),
-          child: const Column(
-            children: [
-              Bulletin(),
-              Bulletin(),
-              Bulletin(),
-              Bulletin(),
-            ],
-          ),
+          child: Container(
+            padding: const EdgeInsets.fromLTRB(0, 5, 0, 5),
+            child: const SizedBox(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('테스트 게시글1', 
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15,
+                        )
+                      ),
+                      Text('2023.09.10 10:00',
+                        style: TextStyle(
+                          fontSize: 13,
+                          color: Colors.grey
+                        )
+                      ),
+                    ],
+                  ),
+                  Text('10분전',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 14,
+                    )
+                  )
+                ],
+              )
+            )
+          )
         )
       ],
     );
   }
 }
 
-class Bulletin extends StatelessWidget {
-  const Bulletin ({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return  Container(
-      padding: const EdgeInsets.fromLTRB(0, 5, 0, 5),
-      child: const SizedBox(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('테스트 게시글1', 
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 15,
-                  )
-                ),
-                Text('2023.09.10 10:00',
-                  style: TextStyle(
-                    fontSize: 13,
-                    color: Colors.grey
-                  )
-                ),
-              ],
-            ),
-            Text('10분전',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 14,
-              )
-            )
-          ],
-        )
-      )
-    );
-  }
-}
